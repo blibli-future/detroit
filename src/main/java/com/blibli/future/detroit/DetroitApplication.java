eaea814b49d81b5e6c749d40e513d3e50f10d7d8
Dito Raharjo <ditoraharjo@gmail.com>
2017-03-07 19:12:15 +0700

    Project Maven Init

---
+++ src/main/java/com/blibli/future/detroit/DetroitApplication.java
@@ -0,0 +1,12 @@
+package com.blibli.future.detroit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetroitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DetroitApplication.class, args);
+	}
+}
---
+++ src/test/java/com/blibli/future/detroit/DetroitApplicationTests.java
@@ -0,0 +1,16 @@
+package com.blibli.future.detroit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DetroitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
