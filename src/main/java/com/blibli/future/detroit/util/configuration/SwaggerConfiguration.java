34c63a11bf94940460d4d45e1b8586bb9fda2cab
Adhika Setya Pramudita <adhika.setyap@gmail.com>
2017-03-11 12:35:28 +0700

    Integrate API with swagger

---
+++ src/main/java/com/blibli/future/detroit/util/configuration/SwaggerConfiguration.java
@@ -0,0 +1,28 @@
+package com.blibli.future.detroit.util.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+@PropertySource("classpath:swagger.properties")
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .pathMapping("/");
+    }
+}
