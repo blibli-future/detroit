207a0afed711eaca679f61b8382c9c928d4fe018
Adhika Setya Pramudita <adhika.setyap@gmail.com>
2017-03-11 11:29:48 +0700

    SimpleListRequest: Create the request object

---
+++ src/main/java/com/blibli/future/detroit/model/request/SimpleListRequest.java
@@ -0,0 +1,19 @@
+package com.blibli.future.detroit.model.request;
+
+import java.util.List;
+
+public class SimpleListRequest<T> {
+    private List<T> list;
+
+    public SimpleListRequest(List<T> list) {
+        this.list = list;
+    }
+
+    public List<T> getList() {
+        return list;
+    }
+
+    public void setList(List<T> list) {
+        this.list = list;
+    }
+}
