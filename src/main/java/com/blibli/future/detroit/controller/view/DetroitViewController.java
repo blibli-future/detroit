b6055ecfcab8cfe83e3f33073ec0b901326fad68
Adhika Setya Pramudita <adhika.setyap@gmail.com>
2017-05-07 17:38:06 +0700

    Frontend: Create simple homepage

---
+++ src/main/java/com/blibli/future/detroit/controller/view/DetroitViewController.java
@@ -0,0 +1,13 @@
+package com.blibli.future.detroit.controller.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DetroitViewController {
+    @RequestMapping("/view")
+    public String homeView(Model model) {
+        return "index";
+    }
+}
