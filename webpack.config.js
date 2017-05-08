019e623fcbb703bf47f2a85d02bcaec985272aee
Adhika Setya Pramudita <adhika.setyap@gmail.com>
2017-05-08 13:12:09 +0700

    Migrate to Webpack v2
    v1 is deprecated

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,30 @@
-var path = require('path');
+const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
     entry: './src/main/frontend/js/app.js',
     devtool: 'sourcemaps',
     cache: true,
-    debug: true,
+    plugins: [
+       new webpack.LoaderOptionsPlugin({
+           debug: true
+       })
+    ],
     output: {
         path: __dirname,
         filename: './src/main/resources/static/bundle.js'
     },
     module: {
-        loaders: [
+        rules: [
             {
                 test: path.join(__dirname, '.'),
                 exclude: /(node_modules)/,
-                loader: 'babel',
+                loader: 'babel-loader',
                 query: {
                     cacheDirectory: true,
                     presets: ['es2015', 'react']
                 }
-            }
+            },
         ]
     }
 };
