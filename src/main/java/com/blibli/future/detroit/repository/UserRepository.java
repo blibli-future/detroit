6b9f14e4d963cfb8096ef08b912807ee182be005
Dito Raharjo <ditoraharjo@gmail.com>
2017-06-18 16:08:00 +0700

    Add query for counting all Agent

--- src/main/java/com/blibli/future/detroit/repository/UserRepository.java
+++ src/main/java/com/blibli/future/detroit/repository/UserRepository.java
@@ -4,10 +4,15 @@ package com.blibli.future.detroit.repository;
 import com.blibli.future.detroit.model.User;
 import com.blibli.future.detroit.model.enums.UserType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.stream.Stream;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findByUserType(UserType userType);
     User findByEmail(String email);
+
+    @Query("select count(u) from User u join u.userRole ur where ur.role='AGENT' ")
+    Integer countAgent();
 }
