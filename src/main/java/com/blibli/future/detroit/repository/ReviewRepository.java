1c82aae86f3f434f6c9c526cadb4730125b818ae
Dito Raharjo <ditoraharjo@gmail.com>
2017-06-18 16:07:47 +0700

    Add query for counting review per user

--- src/main/java/com/blibli/future/detroit/repository/ReviewRepository.java
+++ src/main/java/com/blibli/future/detroit/repository/ReviewRepository.java
@@ -1,9 +1,12 @@
 package com.blibli.future.detroit.repository;
 
+import com.blibli.future.detroit.model.CutOffHistory;
 import com.blibli.future.detroit.model.Parameter;
 import com.blibli.future.detroit.model.Review;
 import com.blibli.future.detroit.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,4 +16,7 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
     List<Review> findByReviewerId(Long reviewerId);
     List<Review> findByAgentId(Long agentId);
     List<Review> findByAgentAndParameter(User agentId, Parameter parameterId);
+    
+    @Query("select count(*) from Review r where (r.reviewer=:reviewerId) and (r.cutOffHistory=:cutOffId) and (r.parameter=:parameterId)")
+    Integer countByReviewer(@Param("reviewerId") User reviewerId, @Param("cutOffId") CutOffHistory cutOffId, @Param("parameterId") Parameter parameterId);
 }
