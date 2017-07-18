b27e6968b70945a4c33236308ed74a553fe8ab9a
adhikasp <adhika.setyap@gmail.com>
2017-07-18 18:52:03 +0700

    Create UserDto and its child
    As alternative to ineficient Response object.

---
+++ src/main/java/com/blibli/future/detroit/model/dto/AgentDto.java
@@ -0,0 +1,44 @@
+package com.blibli.future.detroit.model.dto;
+
+import com.blibli.future.detroit.model.User;
+
+public class AgentDto extends UserDto {
+
+    String teamLeader;
+    String agentChannel;
+    String agentPosition;
+
+    public AgentDto(User user) {
+        super(user);
+        this.teamLeader = user.getTeamLeader();
+        this.agentChannel = user.getAgentChannel().getName();
+        this.agentPosition = user.getAgentPosition().getName();
+    }
+
+
+    // AUTO GENERATED
+
+    public String getTeamLeader() {
+        return teamLeader;
+    }
+
+    public void setTeamLeader(String teamLeader) {
+        this.teamLeader = teamLeader;
+    }
+
+    public String getAgentChannel() {
+        return agentChannel;
+    }
+
+    public void setAgentChannel(String agentChannel) {
+        agentChannel = agentChannel;
+    }
+
+    public String getAgentPosition() {
+        return agentPosition;
+    }
+
+    public void setAgentPosition(String agentPosition) {
+        this.agentPosition = agentPosition;
+    }
+}
---
+++ src/main/java/com/blibli/future/detroit/model/dto/UserDto.java
@@ -0,0 +1,104 @@
+package com.blibli.future.detroit.model.dto;
+
+import com.blibli.future.detroit.model.User;
+import com.blibli.future.detroit.model.enums.UserType;
+
+public class UserDto {
+
+    Long id;
+    String fullname;
+    String nickname;
+    String email;
+    String dateOfBirth;
+    String gender;
+    String location;
+    String phoneNumber;
+    UserType userRole;
+
+    public UserDto(User user) {
+        this.id = user.getId();
+        this.fullname = user.getFullname();
+        this.nickname = user.getNickname();
+        this.email = user.getEmail();
+        this.dateOfBirth = user.getDateOfBirth();
+        this.gender = user.getGender().name();
+        this.location = user.getLocation();
+        this.phoneNumber = user.getPhoneNumber();
+        this.userRole = user.getUserType();
+    }
+
+
+    // AUTO GENERATED
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public UserType getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(UserType userRole) {
+        this.userRole = userRole;
+    }
+}
