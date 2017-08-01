e187a5fd290c70cca4c21f01be9d71cddd88f33c
adhikasp <adhika.setyap@gmail.com>
2017-08-01 17:49:45 +0700

    Create AuthenticationService
    Code taken from blibli-future/blusea

---
+++ src/main/java/com/blibli/future/detroit/service/AuthenticationService.java
@@ -0,0 +1,29 @@
+package com.blibli.future.detroit.service;
+
+import com.blibli.future.detroit.model.User;
+import com.blibli.future.detroit.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class AuthenticationService {
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    public boolean isLoggedIn() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        return auth.isAuthenticated() && !(auth instanceof AnonymousAuthenticationToken) ;
+    }
+
+    public User getCurrentUser() {
+        if (isLoggedIn()) {
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+            return userRepository.findByEmail(auth.getName());
+        }
+        return null;
+    }
+}
