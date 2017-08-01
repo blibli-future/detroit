2e2d359a182e7811f3941705e7aa465aa80b213d
adhikasp <adhika.setyap@gmail.com>
2017-08-01 17:50:17 +0700

    Create NotAuthorizedException
    Used for inappropriate access

---
+++ src/main/java/com/blibli/future/detroit/model/Exception/NotAuthorizedException.java
@@ -0,0 +1,28 @@
+package com.blibli.future.detroit.model.Exception;
+
+public class NotAuthorizedException extends Exception {
+    private final String code = "NOT_AUTHORIZED";
+
+    public NotAuthorizedException() {
+    }
+
+    public NotAuthorizedException(String message) {
+        super(message);
+    }
+
+    public NotAuthorizedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotAuthorizedException(Throwable cause) {
+        super(cause);
+    }
+
+    public NotAuthorizedException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    public String getCode() {
+        return code;
+    }
+}
