6b9007f82f856aad3d410527e31dad4f111a4d67
adhikasp <adhika.setyap@gmail.com>
2017-08-07 16:08:56 +0700

    TopNavigation: Remove user data

--- src/main/frontend/js/containers/TopNavigation.js
+++ src/main/frontend/js/containers/TopNavigation.js
@@ -9,19 +9,6 @@ class TopNavigation extends React.Component{
             <div className="nav toggle">
               <a id="menu_toggle"><i className="fa fa-bars"></i></a>
             </div>
-
-            <ul className="nav navbar-nav navbar-right">
-              <li className="">
-                <a href="javascript:;" className="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                  <img src="images/img.jpg" alt="" />John Doe
-                  <span className=" fa fa-angle-down"></span>
-                </a>
-                <ul className="dropdown-menu dropdown-usermenu pull-right">
-                  <li><a href="login.html"><i className="fa fa-sign-out pull-right"></i> Log Out</a></li>
-                </ul>
-              </li>
-            </ul>
-
           </nav>
         </div>
       </div>
